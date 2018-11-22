6c2f3366c0fec8128b684f6faf92a3367633e971
Theo van Kraay <theo.vankraay@hotmail.com>
2018-11-22 17:28:42 +0000

    Update Function1.cs

--- MongoFunctionHTTP/MongoFunctionHTTP/Function1.cs
+++ MongoFunctionHTTP/MongoFunctionHTTP/Function1.cs
@@ -21,6 +21,10 @@ namespace MongoFunctionHTTP
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
+            //req coming from stream analytics would be in something like this format:
+            //[{"temperature":34.053544718331445,"humidity":73.798910581413153},
+            //{"temperature":25.041046617571752,"humidity":76.803241836281131}]
+                 
             log.LogInformation("C# HTTP trigger function processed a request.");
             const string connectionString = "<connection string of mongodb>";
 
@@ -30,6 +34,7 @@ namespace MongoFunctionHTTP
             //Use the MongoClient to access the server
             var database = client.GetDatabase("mongodb");
 
+            //read the request coming in from stream analytics
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             var messages = JsonConvert.DeserializeObject<List<object>>(requestBody);
 
